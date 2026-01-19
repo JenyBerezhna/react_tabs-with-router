9bfcb17ea90d82ad032b57d92edbb4551f2131d1
Jeny Berezhna <JenySmails24@gmail.com>
2026-01-19 16:23:34 +0000

    add task solution

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,32 +1,24 @@
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
-
+import { Navigate, Route, Routes, HashRouter } from 'react-router-dom';
 import { App } from './App';
 import { Home } from './components/Home';
-import { NotFoundPage } from './components/NotFoundPage';
 import { TabsPage } from './pages/TabsPage';
+import { NotFoundPage } from './components/NotFoundPage';
 
-import 'bulma/css/bulma.css';
-import '@fortawesome/fontawesome-free/css/all.css';
-import './App.scss';
+export const Root = () => (
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
 
-export const Root = () => {
-  return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-
-          {/* Redirect /home -> / */}
-          <Route path="home" element={<Navigate to="/" replace />} />
-
-          <Route path="tabs">
-            <Route index element={<TabsPage />} />
-            <Route path=":tabId" element={<TabsPage />} />
-          </Route>
-
-          <Route path="*" element={<NotFoundPage />} />
+        <Route path="tabs">
+          <Route index element={<TabsPage />} />
+          <Route path=":tabId" element={<TabsPage />} />
         </Route>
-      </Routes>
-    </HashRouter>
-  );
-};
+
+        <Route path="home" element={<Navigate to="/" replace />} />
+
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
+  </HashRouter>
+);
--- src/pages/TabsPage.tsx
+++ src/pages/TabsPage.tsx
@@ -1,20 +1,51 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate, Link } from 'react-router-dom';
+import { Tabs, TabList, Tab, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 import { tabs } from '../types/tabs';
-import { Tabs } from '../components/TabsList';
 
 export const TabsPage = () => {
   const { tabId } = useParams();
-  const selectedTab = tabs.find(t => t.id === tabId);
+  const navigate = useNavigate();
+
+  const selectedIndex = tabs.findIndex(tab => tab.id === tabId);
+  const isValid = selectedIndex !== -1;
+
+  const handleSelect = (index: number) => {
+    navigate(`/tabs/${tabs[index].id}`);
+  };
 
   return (
     <>
       <h1 className="title">Tabs page</h1>
 
-      <Tabs selectedTabId={selectedTab?.id} />
+      <Tabs
+        selectedIndex={isValid ? selectedIndex : -1}
+        onSelect={handleSelect}
+        defaultFocus={false}
+      >
+        <TabList>
+          {tabs.map(tab => (
+            <Tab key={tab.id} data-cy="Tab" selectedClassName="is-active">
+              <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
+            </Tab>
+          ))}
+        </TabList>
 
-      <div className="block" data-cy="TabContent">
-        {selectedTab ? selectedTab.content : 'Please select a tab'}
-      </div>
+        {isValid &&
+          tabs.map(tab => (
+            <TabPanel key={tab.id}>
+              <div className="block" data-cy="TabContent">
+                {tab.content}
+              </div>
+            </TabPanel>
+          ))}
+      </Tabs>
+
+      {!isValid && (
+        <div className="block" data-cy="TabContent">
+          Please select a tab
+        </div>
+      )}
     </>
   );
 };
